ac6310d07896f676e572ef4cae1bbb098106bf4f
Sunilyadav2007 <166435381+Sunilyadav2007@users.noreply.github.com>
2025-05-07 14:51:24 +0530

    Employee.java

--- Employee.java
+++ Employee.java
@@ -2,20 +2,20 @@ public class Employee {
 String name;
 int id;
 double salary;
-public Employee(String name, int id,double salary,int number){
+public Employee(String name, int id,double salary){
     this.name=name;
     this.id=id;
     this.salary=salary;
-    this.number=number;
+  
 }
 public void displayInfo(){
     System.out.println("Employee ID:"+id);
     System.out.println("Employee Name:"+name);
     System.out.println("Salary:"+salary);
-    System.out.println("number:"+number);
+
 }
 public static void main(String[] args) {
-    Employee emp1=new Employee("xyz", 101, 10000.0,11122233);
+    Employee emp1=new Employee("xyz", 101, 10000.0);
     emp1.displayInfo();
 }
 }
